e975931017f6155e38aed885318e05d18e7eeed0
Oleksandr Radkevych <aradkevich78@gmail.com>
2023-12-12 07:12:41 +0500

    #1

--- NET_022_Test2_Task1/Program.cs
+++ NET_022_Test2_Task1/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TestCollection<int> collection = new TestCollection<int>();
+
+            collection.AddItem(12);
+            collection.AddItem(21);
+                
+            collection.AddItem(3);
+            collection.AddItem(43);
+                
+            collection.AddItem(5);
+            collection.AddItem(6);
+
+            Console.WriteLine($"The third item is {collection.GetItem(3)}");
+            
+            var sortedcoll = collection.GetDescSorted();
+
+            for (int i = 0; i < sortedcoll.Count; i++) Console.WriteLine(sortedcoll[i]);
+
+            Console.WriteLine();
         }
     }
 }
---
+++ NET_022_Test2_Task1/TestCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_022_Test2_Task1
+{
+    internal class TestCollection<T> where T:struct 
+    {
+        private List<T> _list;
+
+        public int Count => _list.Count;
+        public TestCollection()
+        {
+            _list = new List<T>();
+        }
+
+        public void AddItem(T item) => _list.Add(item);
+
+        public T GetItem(int id)
+        { 
+            if (id < 0 || id >= _list.Count) throw new ArgumentOutOfRangeException("id");
+
+            return _list[id];
+        }
+
+        public List<T> GetDescSorted()  
+        {
+            return _list.OrderByDescending(item => item).ToList();
+        }
+           
+
+
+        
+        
+    }
+}
